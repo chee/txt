c282df0d2b29f41de42b0a37cbd431e58a44a344
chee <yay@chee.party>
2024-08-03 00:34:11 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,68 @@
+import {registerSW} from "virtual:pwa-register"
+registerSW({immediate: true})
+import {
+	isValidAutomergeUrl,
+	type AutomergeUrl,
+} from "@automerge/automerge-repo/slim"
+import {automergeSyncPlugin} from "@automerge/automerge-codemirror"
+import {EditorView} from "@codemirror/view"
+import {minimalSetup} from "codemirror"
+import {markdown} from "@codemirror/lang-markdown"
+import {dracula} from "@uiw/codemirror-theme-dracula"
+import {BrowserWebSocketClientAdapter} from "@automerge/automerge-repo-network-websocket"
+import {BroadcastChannelNetworkAdapter} from "@automerge/automerge-repo-network-broadcastchannel"
+import {IndexedDBStorageAdapter} from "@automerge/automerge-repo-storage-indexeddb"
+import {Repo} from "@automerge/automerge-repo"
+
+let idb = new IndexedDBStorageAdapter("lb-docs")
+let socky = new BrowserWebSocketClientAdapter(`wss://star.littlebook.app`)
+let tabby = new BroadcastChannelNetworkAdapter()
+let network = [socky, tabby]
+let storage = idb
+let repo = new Repo({
+	network,
+	storage,
+})
+await new Promise<void>(yay => repo.networkSubsystem.once("ready", yay))
+
+async function followHash() {
+	let docUrl = location.hash.slice(1) as AutomergeUrl
+	if (!docUrl || !isValidAutomergeUrl(docUrl)) {
+		docUrl = repo.create({text: ""}).url
+		location.hash = docUrl
+	}
+
+	let docHandle = repo.find<{text: string}>(docUrl)
+	await docHandle.whenReady()
+	return docHandle
+}
+
+let docHandle = await followHash()
+
+let txt = document.getElementById("txt")!
+
+function setupView() {
+	return new EditorView({
+		doc: docHandle.docSync()!.text,
+		extensions: [
+			minimalSetup,
+			automergeSyncPlugin({
+				handle: docHandle,
+				path: ["text"],
+			}),
+			markdown(),
+			dracula,
+		],
+		parent: txt,
+	})
+}
+
+let view = setupView()
+
+window.addEventListener("hashchange", async () => {
+	docHandle = await followHash()
+	view.destroy()
+	view = setupView()
+})
+
+window.repo = repo
---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import {defineConfig, type UserConfig} from "vite"
+import {VitePWA as pwa} from "vite-plugin-pwa"
+import wasm from "vite-plugin-wasm"
+
+export const config: UserConfig = {
+	plugins: [
+		wasm(),
+		pwa({
+			registerType: "prompt",
+			injectRegister: false,
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+			manifest: {
+				name: "txt",
+				short_name: "txt",
+				description: "a text file",
+				theme_color: "#282a36",
+				// todo back and forward buttons
+				display: "fullscreen",
+				background_color: "#282a36",
+			},
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico,wasm}"],
+				cleanupOutdatedCaches: false,
+				clientsClaim: true,
+				maximumFileSizeToCacheInBytes: 999999999999999,
+				additionalManifestEntries: [],
+			},
+			devOptions: {
+				enabled: true,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
+	],
+	build: {
+		outDir: "output",
+		emptyOutDir: true,
+		sourcemap: "hidden",
+		minify: true,
+		target: ["firefox127", "safari17"],
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				includePaths: ["node_modules"],
+			},
+		},
+	},
+}
+
+export default defineConfig(config)
