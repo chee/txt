fe8a9b31c6e97ede0e498491e17cda2de54918fa
chee <yay@chee.party>
2024-08-04 12:17:55 +0100

    add langs to markdown

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,18 @@ import {BrowserWebSocketClientAdapter} from "@automerge/automerge-repo-network-w
 import {BroadcastChannelNetworkAdapter} from "@automerge/automerge-repo-network-broadcastchannel"
 import {IndexedDBStorageAdapter} from "@automerge/automerge-repo-storage-indexeddb"
 import {Repo} from "@automerge/automerge-repo"
+import {
+	LanguageDescription,
+	indentUnit,
+	type LanguageSupport,
+} from "@codemirror/language"
 
 let idb = new IndexedDBStorageAdapter("lb-docs")
-// let socky = new BrowserWebSocketClientAdapter(`wss://star.littlebook.app`)
-let socky2 = new BrowserWebSocketClientAdapter(
+let socky = new BrowserWebSocketClientAdapter(
 	`wss://autosync-rdd6.onrender.com`
 )
 let tabby = new BroadcastChannelNetworkAdapter()
-let network = [socky2, tabby]
+let network = [socky, tabby]
 let storage = idb
 let repo = new Repo({
 	network,
@@ -53,7 +57,88 @@ function setupView() {
 				handle: docHandle,
 				path: ["text"],
 			}),
-			markdown(),
+			markdown({
+				codeLanguages: [
+					LanguageDescription.of({
+						name: "javascript",
+						alias: ["js", "jsx", "ts", "tsx", "typescript"],
+						filename: /\.[jt]sx?$/,
+						async load() {
+							return import("@codemirror/lang-javascript").then(mod =>
+								mod.javascript()
+							)
+						},
+					}),
+					LanguageDescription.of({
+						name: "html",
+						filename: /\.html$/,
+						async load() {
+							return import("@codemirror/lang-html").then(mod => mod.html())
+						},
+					}),
+					LanguageDescription.of({
+						name: "python",
+						alias: ["py"],
+						filename: /\.py$/,
+						async load() {
+							return import("@codemirror/lang-python").then(mod => mod.python())
+						},
+					}),
+					LanguageDescription.of({
+						name: "css",
+						alias: ["style"],
+						filename: /\.css$/,
+						async load() {
+							return import("@codemirror/lang-css").then(mod => mod.css())
+						},
+					}),
+					LanguageDescription.of({
+						name: "sass",
+						alias: ["scss"],
+						filename: /\.s[ca]ss$/,
+						async load() {
+							return import("@codemirror/lang-sass").then(mod => mod.sass())
+						},
+					}),
+					LanguageDescription.of({
+						name: "sql",
+						filename: /\.sql$/,
+						async load() {
+							return import("@codemirror/lang-sql").then(mod => mod.sql())
+						},
+					}),
+					LanguageDescription.of({
+						name: "go",
+						filename: /\.go$/,
+						async load() {
+							return import("@codemirror/lang-go").then(mod => mod.go())
+						},
+					}),
+					LanguageDescription.of({
+						name: "json",
+						filename: /\.json$/,
+						async load() {
+							return import("@codemirror/lang-json").then(mod => mod.json())
+						},
+					}),
+					LanguageDescription.of({
+						name: "rust",
+						alias: ["rs"],
+						filename: /\.rust$/,
+						async load() {
+							return import("@codemirror/lang-rust").then(mod => mod.rust())
+						},
+					}),
+					LanguageDescription.of({
+						name: "cpp",
+						alias: ["c"],
+						filename: /\.c([px]{2})?$/,
+						async load() {
+							return import("@codemirror/lang-cpp").then(mod => mod.cpp())
+						},
+					}),
+				],
+			}),
 			dracula,
 		],
 		parent: txt,
