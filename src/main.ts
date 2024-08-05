59bac6a797f694a000e4312df49c07d6be861c77
chee <yay@chee.party>
2024-08-05 15:08:18 +0100

    add many cursors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ registerSW({immediate: true})
 import {
 	isValidAutomergeUrl,
 	type AutomergeUrl,
+	type PeerId,
 } from "@automerge/automerge-repo/slim"
 import {automergeSyncPlugin} from "@automerge/automerge-codemirror"
-import {EditorView} from "@codemirror/view"
+import {
+	Decoration,
+	EditorView,
+	type DecorationSet,
+	type ViewUpdate,
+} from "@codemirror/view"
 import {minimalSetup} from "codemirror"
 import {markdown} from "@codemirror/lang-markdown"
 import {dracula} from "@uiw/codemirror-theme-dracula"
@@ -14,6 +20,8 @@ import {BroadcastChannelNetworkAdapter} from "@automerge/automerge-repo-network-
 import {IndexedDBStorageAdapter} from "@automerge/automerge-repo-storage-indexeddb"
 import {Repo} from "@automerge/automerge-repo"
 import {LanguageDescription} from "@codemirror/language"
+import {Tooltip, showTooltip} from "@codemirror/view"
+import {StateField, EditorState} from "@codemirror/state"
 
 let txt = document.getElementById("txt")!
 
@@ -24,17 +32,25 @@ for (let [flag, value] of featureflags.entries()) {
 if (featureflags.has("rtl")) {
 	txt.style.direction = "rtl"
 }
+if (featureflags.has("name")) {
+	localStorage.setItem("name", featureflags.get("name") ?? "")
+}
+if (featureflags.has("name")) {
+	localStorage.setItem("name", featureflags.get("name") ?? "")
+}
 
 let idb = new IndexedDBStorageAdapter("lb-docs")
 let socky = new BrowserWebSocketClientAdapter(
 	`wss://autosync-rdd6.onrender.com`
 )
+
 let tabby = new BroadcastChannelNetworkAdapter()
 let network = [socky, tabby]
 let storage = idb
 let repo = new Repo({
 	network,
 	storage,
+	peerId: (localStorage.getItem("name") as PeerId) ?? undefined,
 })
 await new Promise<void>(yay => repo.networkSubsystem.once("ready", yay))
 
@@ -47,15 +63,139 @@ async function followHash() {
 
 	let docHandle = repo.find<{text: string}>(docUrl)
 	await docHandle.whenReady()
+
 	return docHandle
 }
 
 let docHandle = await followHash()
 
+type Range = {
+	from: number
+	to: number
+	head: number
+	anchor: number
+}
+
+type RangeMessage = Range & {$type: "range"}
+type HelloMessage = {$type: "hello"}
+type AnyMessage = RangeMessage | HelloMessage
+
+function broadcast(state: EditorState) {
+	for (let range of state.selection.ranges) {
+		docHandle.broadcast({
+			$type: "range",
+			from: range.from,
+			to: range.to,
+			head: range.head,
+			anchor: range.anchor,
+		} satisfies RangeMessage)
+	}
+}
+
+function ephemera(update: ViewUpdate) {
+	if (update.selectionSet) {
+		broadcast(update.state)
+	}
+}
+
+function cursors() {
+	let friends: Record<string, Range & {time: number}> = {}
+	let friendRangeMark = Decoration.mark({
+		class: "cm-friend cm-friend-range",
+	})
+	let friendPointMark = Decoration.widget({
+		widget: {
+			eq(widget) {
+				return widget == this
+			},
+			updateDOM(dom, view) {
+				return true
+			},
+			ignoreEvent(event) {
+				return true
+			},
+			estimatedHeight: -1,
+			lineBreaks: 0,
+			coordsAt(dom, pos, side) {
+				return {bottom: 0, left: 0, right: 0, top: 0}
+			},
+			toDOM(view) {
+				let span = document.createElement("span")
+				span.className = "cm-friend cm-friend-point"
+				return span
+			},
+			destroy() {
+				return
+			},
+		},
+	})
+	friendPointMark.point = true
+
+	return StateField.define<DecorationSet>({
+		create(state) {
+			docHandle.on("ephemeral-message", payload => {
+				let id = payload.senderId
+				let message = payload.message as AnyMessage
+				if (message.$type == "range") {
+					friends[id] = {
+						...message,
+						time: Date.now(),
+					}
+				}
+				if (message.$type == "hello") {
+					broadcast(state)
+				}
+			})
+			docHandle.broadcast({type: "$hello"})
+			return Decoration.none
+		},
+
+		update(marks, tr) {
+			marks.map(tr.changes)
+			marks = marks.update({
+				filter() {
+					return false
+				},
+			})
+
+			for (let [id, range] of Object.entries(friends)) {
+				console.log(Date.now() - range.time > 2000, Date.now(), range.time)
+				if (Date.now() - range.time > 2000) {
+					delete friends[id]
+					continue
+				}
+				let mark =
+					range.from == range.to
+						? friendPointMark.range(range.from)
+						: friendRangeMark.range(range.from, range.to)
+				mark.value.spec.id = id
+				marks = marks.update({
+					add: [mark],
+				})
+			}
+
+			marks = marks.update({
+				filter(from, to, value) {
+					if (value.spec.id in friends) {
+						return true
+					}
+					return false
+				},
+			})
+
+			return marks
+		},
+
+		provide: f => EditorView.decorations.from(f),
+	})
+}
+
 function setupView() {
 	return new EditorView({
 		doc: docHandle.docSync()!.text,
 		extensions: [
+			EditorView.updateListener.of(ephemera),
+			cursors(),
 			minimalSetup,
 			automergeSyncPlugin({
 				handle: docHandle,
@@ -150,6 +290,10 @@ function setupView() {
 }
 
 let view = setupView()
+
+setInterval(() => {
+	broadcast(view.state)
+}, 1000)
 
 view.focus()
 
